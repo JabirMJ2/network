d83fe5c061bd28eb9155bf136fbc2eb729f21dbf
JabirMJ2 <jabirmj1@gmail.com>
2022-05-17 16:13:29 +0530

    16-5-2022

--- switch2.sh
+++ switch2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Enter operation 1.add 2.sub"
+echo "Enter operation 1.add 2.sub 3.product 3.quotient 5.remainder"
 read choice
 
 n1=10
@@ -12,6 +12,15 @@ case "$choice" in
 	
 	"2")
 	echo `expr $n1 - $n2`
+	;;
+	"3")
+	echo `expr $n1 \* $n2`
+	;;
+	"4")
+	echo `expr $n1 / $n2`
+	;;
+	"5")
+	echo `expr $n1 % $n2`
 	;;
 	*)
 	echo "Invalid Choice"
